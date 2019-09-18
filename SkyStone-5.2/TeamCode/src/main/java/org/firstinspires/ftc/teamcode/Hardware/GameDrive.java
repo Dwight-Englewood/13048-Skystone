75ed7cad84b9445599ab04dd4cdad9d528eab965
Joonsoo Lee <joonsoo@Joonsoos-MacBook-Pro.local>
2019-09-18 15:48:28 -0400

    Made Tank and Game Drive files

---
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/GameDrive.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.*;
+import com.vuforia.CameraDevice;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class GameDrive {
+
+    public Bot bot;
+
+    public GameDrive(Bot bot){
+        this.bot = bot;
+    }
+}
---
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/TankDrive.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Hardware;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.*;
+import com.vuforia.CameraDevice;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class TankDrive {
+
+    public Bot bot;
+
+    public TankDrive(Bot bot){
+        this.bot = bot;
+    }
+}
