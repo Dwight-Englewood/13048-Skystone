415cf35adfb21b59f2a5f987c805407483ab22b2
Royce Moon <royce.moon@Royces-MacBook-Air.local>
2019-09-19 14:59:51 -0400

    reee

--- SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/TankDrive.java
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/TankDrive.java
@@ -12,12 +12,29 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import java.lang.Math;
 
 public class TankDrive {
 
     public Bot bot;
 
-    public TankDrive(Bot bot){
+    public TankDrive(Bot bot) {
         this.bot = bot;
+
+        public static void Drive ( double leftStickY, double rightStickX, double rightTrigger,
+        double leftTrigger, double rightStickY, double leftStickX){
+            bot.drivePower(leftStickY);
+            bot.drivePower(rightStickX);
+            double StickyPressure = .5;
+
+            if (leftStickY > Math.abs(StickyPressure) && rightStickY > Math.abs(StickyPressure)) {
+                bot.drivePower((leftStickY + rightStickY) / 2.00);
+            }
+            if (leftStickX > Math.abs(StickyPressure) && rightStickX > Math.abs(StickyPressure)) {
+                bot.strafePower((leftStickX + rightStickX) / 2.00);
+            })
+
+
+        }
     }
 }
